0150a70244f6349386899f67f269e997ba69ad4d
Javier Hernanz Zajara <javierhz@server.javierhz.private>
2014-06-18 01:25:12 +0200

    It seems to work. Test more and report directly if working properly (or solve if any issue is detected).

--- cachematrix.R
+++ cachematrix.R
@@ -4,28 +4,36 @@
 ## @write a pair of functions that cache the inverse of a matrix.
 
 # Tip for testing:
-#     m <- replicate(5,norm(5))     # Random 5x5 matrix
+#     m <- replicate(5, rnorm(5))   # Random 5x5 matrix
 #     solve(m)                      # Inverse matrix of m
+
+# Test code:
+#  
+#  A <- replicate(5000, rnorm(5000))
+#  a <- makeCacheMatrix(A)
+#  cacheSolve(a)
+#  A <- replicate(5000, rnorm(5000))
+#  cacheSolve(a) # It should be faster if properly cached
+#  a <- makeCacheMatrix(A)
+#  cacheSolve(a) # As a has changed it should be slow again
+#  cacheSolve(a) # cached!
+
+
 
 ## makeCacheMatrix: creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-  
-  ## Sample code from the assignment (using a mean value)
   m <- NULL
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-  
-  
-  ##### My code follows... (I hope) ####
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
@@ -34,23 +42,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ##    should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
   
-  ## Sample code from the assignment (using a mean value)
-  
-  cachemean <- function(x, ...) {
-    m <- x$getmean()
+    m <- x$getsolve()
     if(!is.null(m)) {
       message("getting cached data")
       return(m)
     }
     data <- x$get()
-    m <- mean(data, ...)
-    x$setmean(m)
+    m <- solve(data, ...)
+    x$setsolve(m)
     m
-  }
-  
-  ##### My code follows... (I hope) ####
-  
   
 }
